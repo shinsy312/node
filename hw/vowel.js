cfb3c081518d7d9dc981a23ab793a4ad4a1d1ae1
seoyoung <shsy312@naver.com>
2020-03-31 17:57:25 +0900

    hw2

---
+++ hw/vowel.js
@@ -0,0 +1,7 @@
+let a = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
+
+console.log("a의 인덱스: "+a.search(/[a]/ig))
+console.log("e의 인덱스: "+a.search(/[e]/ig))
+console.log("i의 인덱스: "+a.search(/[i]/ig))
+console.log("o의 인덱스: "+a.search(/[o]/ig))
+console.log("u의 인덱스: "+a.search(/[u]/ig))
